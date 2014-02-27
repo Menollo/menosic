cbad3940f427777ab508c24174cf55c47666c229
Jacco Koning <orval@menollo.nl>
2014-02-27 14:29:38 +0100

    convert non-mp3 files to mp3

--- menosic/music/views.py
+++ menosic/music/views.py
@@ -2,9 +2,11 @@ import mimetypes
 
 from django.views.generic import ListView, DetailView, View
 from django.views.generic.detail import SingleObjectMixin
-from django.http import HttpResponse
+from django.http import HttpResponse, StreamingHttpResponse
 #from django.core.servers.basehttp import FileWrapper
 from django.conf import settings
+import subprocess
+import os
 
 from music import models
 
@@ -20,18 +22,35 @@ class AlbumDetailView(DetailView):
 class TrackDetailView(DetailView):
     model = models.Track
 
+
 class TrackView(SingleObjectMixin, View):
     model = models.Track
 
     def get(self, request, *args, **kwargs):
         track = self.get_object()
 
-        response = HttpResponse(content_type=track.mimetype)
+        if track.mimetype == 'audio/mpeg':
+            response = HttpResponse(content_type='audio/mpeg')
+            if settings.DEBUG:
+                response.write(open(track.path, 'rb').read()) # FileWrapper?
+            else:
+                response['X-Accel-Redirect'] = track.path
 
-        if settings.DEBUG:
-            response.write(open(track.path, 'rb').read()) # FileWrapper?
         else:
-            response['X-Accel-Redirect'] = track.path
-
+            if track.mimetype == 'audio/x-flac':
+                flac = subprocess.Popen(
+                    ["flac", "--silent", "--decode", "--stdout", track.path],
+                    stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+                mp3 = subprocess.Popen(
+                    ["lame", "--silent", "-h", "-b", "192", "-"],
+                    stdin=flac.stdout, stdout=subprocess.PIPE)
+                response = StreamingHttpResponse(mp3.stdout, content_type='audio/mpeg')
+            else:
+                pipe = subprocess.Popen(
+                        ["ffmpeg", "-i", track.path, "-ab", "192k", "-v", "0", "-f", "mp3", "-"],
+                        stdin=subprocess.PIPE,
+                        stdout=subprocess.PIPE
+                    )
+                response = StreamingHttpResponse(pipe.stdout, content_type='audio/mpeg')
         return response
     
